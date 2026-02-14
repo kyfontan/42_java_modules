2f19d09ede64ab1112ad050e1174b24cad91f4ef
Kyllian Fontanille <kyfontan@student.42perpignan.fr>
2026-02-14 19:55:19 +0100

    First commit

---
+++ ex00/Program.java
@@ -0,0 +1,11 @@
+
+
+public class Program{
+    public static void main(String[] args){
+        int res = 0;
+        for (String arg: args){
+            res += Integer.parseInt(arg);
+        }
+        System.out.println(res);
+    }
+}
